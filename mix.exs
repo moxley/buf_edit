b6bf1df779416a04df82fc672ba8a5828a76afeb
Moxley Stratton <moxley.stratton@gmail.com>
2018-08-24 09:09:29 -0700

    Update ex_doc

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule BufEdit.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 
